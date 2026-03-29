4ca4a8387b1ba65de463c88a89c19dccd2bc34a7
Shashank <shashank3fr@gmail.com>
2026-03-29 23:59:19 +0530

    add db  configuration file and env variables

---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({   
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
+});
+
+export default pool;
