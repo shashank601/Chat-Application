ea40eeba12d522f6419f584732ebfb5820f7ce7c
Shashank <shashank3fr@gmail.com>
2026-03-30 00:04:00 +0530

    fix shadowing conflicts and make errors msg  generic

--- backend/src/controllers/auth_controllers.js
+++ backend/src/controllers/auth_controllers.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import pool from '../config/db.js';
 import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/env.js';
 import { async_handler } from '../middlewares/async_handler.js';
-import { login, register } from '../db/queries.js'
+import { login_query, register_query } from '../db/queries.js'
 
 export const login = async_handler(async (req, res) => {
     const { email, password } = req.body;
@@ -13,18 +13,18 @@ export const login = async_handler(async (req, res) => {
         throw err;
     }
 
-    const result = await pool.query(login, [email]); // get hashed pwd to compare
+    const result = await pool.query(login_query, [email]); // get hashed pwd to compare
 
     if (result.rows.length === 0) {
-        const err = new Error('email not found');
-        err.statusCode = 404;
+        const err = new Error('invalid credentials');
+        err.statusCode = 401;
         throw err;
     }
 
     const user = result.rows[0];
     const isMatch = await bcrypt.compare(password, user.pwd);
     if (!isMatch) {
-        const err = new Error('Invalid pwd');
+        const err = new Error('Invalid credentials');
         err.statusCode = 401;
         throw err;
     }
@@ -35,7 +35,7 @@ export const login = async_handler(async (req, res) => {
         { expiresIn: JWT_EXPIRES_IN }
     );
 
-    res.json({ token });
+    res.status(200).json({ token });
 });
 
 
@@ -53,7 +53,7 @@ export const register = async_handler(async (req, res) => {
     const hashedpwd = await bcrypt.hash(password, salt);
     let result;
     try {
-        result = await pool.query(register, [name, email, hashedpwd]);
+        result = await pool.query(register_query, [name, email, hashedpwd]);
     } catch (err) {
         if (err.code === '23505') {  // unique_violation
             err.statusCode = 409;
@@ -69,7 +69,7 @@ export const register = async_handler(async (req, res) => {
         { expiresIn: JWT_EXPIRES_IN }
     );
 
-    res.json({ token });
+    res.status(201).json({ token });
     
     
 });
