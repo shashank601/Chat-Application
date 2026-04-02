b90577ecd5d3821416259f967e1910f75469a8c7
Shashank <shashank3fr@gmail.com>
2026-04-03 00:42:07 +0530

    apply verify token middleware to protected routes

--- backend/src/routes/index_routes.js
+++ backend/src/routes/index_routes.js
@@ -3,12 +3,13 @@ import auth_routes from './auth_routes.js';
 import messages_routes from './messages_routes.js';
 import room_routes from './room_routes.js';
 import user_routes from './user_routes.js';
+import verify_token from '../middlewares/verify_token.js';
 
 const router = express.Router();
 
 router.use('/auth', auth_routes);
-router.use('/messages', messages_routes);
-router.use('/rooms', room_routes);
-router.use('/users', user_routes);
+router.use('/messages', verify_token, messages_routes);
+router.use('/rooms', verify_token, room_routes);
+router.use('/users', verify_token, user_routes);
 
 export default router;
