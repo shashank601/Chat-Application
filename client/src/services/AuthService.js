a62fd840ca6b6c3e180a3f1c1146eb42ef11a182
Shashank <shashank3fr@gmail.com>
2026-04-05 15:04:31 +0530

    add Auth Service

---
+++ client/src/services/AuthService.js
@@ -0,0 +1,12 @@
+import api from "./Axios.js";
+
+
+export const Login = ({email, password}) => {
+    const credentials = {email, password};
+    return api.post('/auth/login', credentials); // jwt token or error
+}
+
+export const Signup = ({username, email, password}) => { 
+    const credentials = {username, email, password};
+    return api.post('/auth/register', credentials); // {id, username, email} or error
+}
