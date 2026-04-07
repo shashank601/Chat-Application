03a0c5b2c43ec67af5c39b485bb1650d5877e88f
Shashank <shashank3fr@gmail.com>
2026-04-07 11:50:47 +0530

    fix imports

--- backend/src/routes/auth_routes.js
+++ backend/src/routes/auth_routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { register, login, verify } from '../controllers/auth_controllers.js';
-import { verify_token } from '../middlewares/auth_middleware.js';
+import verify_token from '../middlewares/verify_token.js';
 
 const router = express.Router();
 
