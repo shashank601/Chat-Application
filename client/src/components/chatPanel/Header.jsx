54e47ddb148b52201db1bb147808760c3c5fff78
Shashank <shashank3fr@gmail.com>
2026-04-11 00:11:40 +0530

    imporve header and artist credits bar

--- client/src/components/chatPanel/Header.jsx
+++ client/src/components/chatPanel/Header.jsx
@@ -3,9 +3,9 @@ import { useParams } from "react-router-dom";
 export default function Header() {
   const { displayName } = useParams();
   return (
-    <ul className="flex sticky top-0 justify-between items-center p-4 bg-zinc-900 w-full">
-      <li className="font-serif text-white">{displayName}</li>
-      <li className="text-white cursor-pointer bg-slate-100 hover:bg-red-500 p-2 rounded-lg hover:animate-pulse">
+    <ul className="flex sticky top-0 justify-between items-center p-1 bg-zinc-900 w-full">
+      <li className="font-serif text-[#c0d6c2] font-bold text-xl">{displayName}</li>
+      <li className="text-white cursor-pointer bg-slate-100 hover:bg-red-500 px-1 py-1 rounded-lg hover:animate-pulse">
         <img src="../../../assets/delete.svg" alt="delete" />
       </li>
     </ul>
--- client/src/containers/ChatPanel.jsx
+++ client/src/containers/ChatPanel.jsx
@@ -65,10 +65,11 @@ export default function ChatPanel() {
         <div className="flex flex-col justify-start">
 
           <a
-            className="text-[#c0d6c2] text-sm"
+            className="text-[#000] text-[10px]  bg-gradient-to-r from-slate-200 "
             href="https://www.freepik.com/free-vector/app-icon-doodle-pattern_363621845.htm#fromView=search&page=3&position=44&uuid=e8c953a4-381c-4aeb-abd0-b0f4890a4988&query=chat+pattern"
             >
-            Image by vector_corp on Freepik
+              <span className="font-bold">credits: </span>
+              <span>Image by vector_corp on Freepik</span>
           </a>
 
           {loading ? (
