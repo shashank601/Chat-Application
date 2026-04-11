83a4db59729230a03b2d73e8e7f580d0c2d84cfa
Shashank <shashank3fr@gmail.com>
2026-04-11 10:21:36 +0530

    add micro erase icon on bubble and add white version of eraser svg

--- client/src/components/cards/Bubble.jsx
+++ client/src/components/cards/Bubble.jsx
@@ -6,7 +6,12 @@ export default function Bubble({ senderName, content, createdAt, isMine }) {
           isMine ? " rounded-tr-none" : " rounded-tl-none"
         }`}
       >
-        <div className="font-serif text-[12px] text-gray-300">{senderName}</div>
+        <div className="flex justify-between items-center gap-1">
+          <div className="font-serif text-[12px] text-gray-300">{senderName}</div>
+          <div>
+            <img src="../../../assets/chatDeleteWhite.svg" alt="delete" className="w-3 h-3 text-slate-100 hover:opacity-80 cursor-pointer" />
+          </div>
+        </div>
 
         <div className="text-white text-center">{content}</div>
 
