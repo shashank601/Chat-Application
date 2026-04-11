4f26ea9c4bb939d93579144f2ae5590bf0c993ae
Shashank <shashank3fr@gmail.com>
2026-04-11 12:20:37 +0530

    add input bar for group name

--- client/src/containers/SidebarContainer.jsx
+++ client/src/containers/SidebarContainer.jsx
@@ -13,7 +13,7 @@ export default function SidebarContainer() {
   const navigate = useNavigate();
   const { user } = useAuth();
   //console.log(`User: ${Object.keys(user)}`); i have  only id in context no username!
-  
+
   const userId = user?.id || "";
 
   const [rooms, setRooms] = useState([]);
@@ -36,7 +36,13 @@ export default function SidebarContainer() {
       setRooms((prevRooms) => {
         return prevRooms
           .map((room) =>
-            room.room_id === msg.room_id ? { ...room, last_msg: msg.content, last_msg_at: new Date(msg.created_at).getTime() } : room,
+            room.room_id === msg.room_id
+              ? {
+                  ...room,
+                  last_msg: msg.content,
+                  last_msg_at: new Date(msg.created_at).getTime(),
+                }
+              : room,
           )
           .sort((a, b) => {
             const aTime = a.last_msg_at || 0;
@@ -75,20 +81,57 @@ export default function SidebarContainer() {
     return () => remove();
   }, [onRoomDeleted]);
 
+  const createRoomHandler = async () => {
+    setDisplay(false);
+    await createRoom(null, groupName); // no user intially so null later add users one by one
+    fetchRooms();
+  };
+
+  const [display, setDisplay] = useState(false);
+  const [groupName, setGroupName] = useState("");
+
   return (
     <div className="flex flex-col min-h-screen ml-10 mr-4">
       <div className="h-1/5 sticky top-0 bg-slate-50 pb-3">
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold pb-4">Live Chat</h1>
-          <h1
-            className="text-2xl font-bold pb-1 mb-2 fixed left-1 bottom-1 cursor-pointer border border-gray-300 rounded-full w-8 h-8 flex items-center justify-center hover:bg-gray-100"
-            onClick={async () => {
-              await createRoom(null, "hi");
-              fetchRooms();
-            }}
-          >
-            +
-          </h1>
+          <div className="relative">
+            {!display && (
+              <button
+                onClick={() => setDisplay(true)}
+                className="text-2xl font-bold pb-1 mb-2 fixed left-1 bottom-1 cursor-pointer border border-gray-300 rounded-full w-8 h-8 flex items-center justify-center hover:bg-gray-100 bg-white z-10"
+              >
+                +
+              </button>
+            )}
+            {display && (
+              <div className="fixed left-1 bottom-1 flex gap-2 items-center bg-white p-2 border border-gray-300 rounded-lg z-10">
+                <input
+                  className="border border-zinc-900 px-2 py-1 rounded outline-none focus:outline-none focus:ring-2 focus:ring-zinc-900"
+                  value={groupName}
+                  onChange={(e) => setGroupName(e.target.value)}
+                  type="text"
+                  placeholder="Room name"
+                  autoFocus
+                />
+                <button
+                  onClick={createRoomHandler}
+                  className="px-3 py-1 bg-zinc-900 text-white rounded hover:bg-zinc-700"
+                >
+                  Create
+                </button>
+                <button
+                  onClick={() => {
+                    setDisplay(false);
+                    setGroupName("");
+                  }}
+                  className="px-2.5 pb-1 text-gray-500 hover:text-gray-700 font-bold text-xl rounded-full border-1"
+                >
+                  x
+                </button>
+              </div>
+            )}
+          </div>
         </div>
         <Searchbar />
       </div>
