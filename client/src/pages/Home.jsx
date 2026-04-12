fcab3cea635f5a8bb81b17ae71a1811a9c06f01d
Shashank <shashank3fr@gmail.com>
2026-04-12 21:29:59 +0530

    add a  sneaky msg 👻

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,10 @@
 export default function Home() {
   return (
     <div className="h-full w-full bg-zinc-900">
-      <div className="flex justify-center items-center h-screen w-full">
+      <div className="flex flex-col justify-center items-center h-screen w-full">
         
         <p className="text-2xl font-bold text-slate-100 px-4 py-2 rounded-lg animate-pulse">Join a Chat to get Realtime messages</p>
+        <p className="text-sm text-zinc-800 px-4 py-2 rounded-lg hover:text-slate-800 hover:font-semibold"><span className="animate-ping ">NOTE: </span>sidebar last_message only updates if you explicitly open that chat, this app's developer didnt like the idea of joining all chats automatically</p>
       </div>
     </div>
   );
