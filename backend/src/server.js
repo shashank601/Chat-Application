1d7c96d99648a9d4db44a41277ea0525956b4692
Shashank <shashank3fr@gmail.com>
2026-04-20 13:37:59 +0530

    fix wildcard path

--- backend/src/server.js
+++ backend/src/server.js
@@ -72,7 +72,7 @@ app.use(express.static(staticPath));
 
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-app.get("*", (req, res, next) => {
+app.get(/(.*)/, (req, res, next) => {
   res.sendFile(path.join(staticPath, "index.html"));
 });
 
